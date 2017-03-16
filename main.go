eb228d173d9564452a0691cf2f8fc48f658fbbf4
edgar <edgar@corma.com.tw>
2017-03-16 13:46:54 +0800

    no message

--- main.go
+++ main.go
@@ -49,11 +49,11 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 		if event.Type == linebot.EventTypeMessage {
 			switch message := event.Message.(type) {
 			case *linebot.TextMessage:
-				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.ID+":"+message.Text+" TextOK!")).Do(); err != nil {
+				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.ID+":"+message.Text+"TextOK!")).Do(); err != nil {
 					log.Print(err)
 				}
 			case *linebot.ImageMessage:
-				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewImageMessage(message.ID+":/n"+message.OriginalContentURL+"/n"+PreviewImageURL+"/n ImageOK!")).Do(); err != nil {
+				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewImageMessage(message.ID+":"+message.OriginalContentURL+PreviewImageURL+"ImageOK!")).Do(); err != nil {
 					log.Print(err)
 				}
 			}
