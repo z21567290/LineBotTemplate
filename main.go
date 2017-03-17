f9a77041cba0b457183c3a25fe54f21a0c987c60
edgar <edgar@corma.com.tw>
2017-03-17 13:53:57 +0800

    no message

--- main.go
+++ main.go
@@ -65,22 +65,22 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 			switch message := event.Message.(type) {
 
 			case *linebot.TextMessage:
-				inText := strings.ToLower(message.Text)
+				inText := message.Text
 				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.ID+":"+message.Text+" TextOK!")).Do(); err != nil {
 					//發送訊息的格式
 					log.Print(err)
 				}
-				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(inText)).Do(); err != nil && {
+				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(inText)).Do(); err != nil {
 					log.Print(err)
 				}
-				/*out := fmt.Sprintf("謝謝愛我 ，但LINEBOT依然機掰")
+				out := fmt.Sprintf("謝謝愛我 ，但LINEBOT依然機掰")
 				if strings.Contains(inText, "愛你") { //_, err2 =
 					//out := fmt.Sprintf("謝謝愛我 ，但LINEBOT依然機掰")
-					//if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(out)).Do(); err != nil {
+					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(out)).Do(); err != nil {
 					//log.Print(err)
 					//}
 				}
-				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(out)).Do(); err != nil && {
+				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(out)).Do(); err != nil {
 					log.Print(err)
 				}*/
 
