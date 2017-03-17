d23c3bdd34cd880f4b4191a95266e0fe09d4ea15
edgar <edgar@corma.com.tw>
2017-03-17 10:56:27 +0800

    no message

--- main.go
+++ main.go
@@ -67,12 +67,17 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 					//發送訊息的格式
 					log.Print(err)
 				}
-				if strings.Contains(message.Text == "我愛你") {
-					out := fmt.Sprintf("謝謝愛我 ，但LINEBOT依然機掰")
+				out := fmt.Sprintf("謝謝愛我 ，但LINEBOT依然機掰")
+				if strings.Contains(message.Text == "愛你") {
+					//out := fmt.Sprintf("謝謝愛我 ，但LINEBOT依然機掰")
+					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(out)).Do(); err != nil {
+						log.Print(err)
+					}
 				}
-				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(out)).Do(); err != nil {
+				/*if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(out)).Do(); err != nil {
 					log.Print(err)
-				}
+				}*/
+
 				/*
 					if _, err := bot.PushMessage(<to>, linebot.NewTextMessage("hello")).Do(); err != nil {
 					    ...
