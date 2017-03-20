fbb04759a61243d1ae363462e5206d3460f47704
edgar <edgar@corma.com.tw>
2017-03-20 18:56:42 +0800

    no message

--- main.go
+++ main.go
@@ -83,10 +83,13 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 					bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(out)).Do()
 					//bot.ReplyMessage(event.ReplyToken, linebot.NewImageMessage(message.OriginalContentURL+message.PreviewImageURL)).Do()
 				} else if strings.Contains(message.Text, "全頻廣播") {
-					IP := "6c65c8b36882491faa32493bfeba736" //飲茶
+
+					//IP := event.ReplyToken //飲茶
 					//IP[1] = "b4c929b7ceec4e21912e6e16304ff0ee" //台南吃吃吃
 					//IP := []string{"6c65c8b36882491faa32493bfeba736", "b4c929b7ceec4e21912e6e16304ff0ee"}
 					//IPP := IP[2]
+					bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("*注意*")).Do()
+					IP := event.ReplyToken
 					bot.PushMessage(IP, linebot.NewTextMessage("hello")).Do()
 				}
 
