623fcbeb9cfca50a8bfd466d1c6d8a84017bce8d
edgar <edgar@corma.com.tw>
2017-03-20 18:38:00 +0800

    no message

--- main.go
+++ main.go
@@ -86,8 +86,8 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 					IP[0] = "6c65c8b36882491faa32493bfeba736"  //飲茶
 					IP[1] = "b4c929b7ceec4e21912e6e16304ff0ee" //台南吃吃吃
 					//IP[2] ={"6c65c8b36882491faa32493bfeba736","b4c929b7ceec4e21912e6e16304ff0ee"}
-					IPP := IP[2]
-					bot.PushMessage(IPP, linebot.NewTextMessage("hello")).Do()
+					//IPP := IP[2]
+					bot.PushMessage(IP, linebot.NewTextMessage("hello")).Do()
 				}
 
 				/* else if strings.Contains(message.Text, "rdrrJC") {
