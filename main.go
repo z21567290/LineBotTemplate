301c58e931aba928cf3d105aca3a30f1dedac80d
edgar <edgar@corma.com.tw>
2017-03-20 12:08:11 +0800

    no message

--- main.go
+++ main.go
@@ -96,10 +96,10 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 			//----------------------------------------------------------------------
 
 			case *linebot.ImageMessage:
-				if message.ID == "RS232.jpg" {
-					out := fmt.Sprintf("這是RS232接頭")
-					bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(out+message.OriginalContentURL+message.PreviewImageURL)).Do()
-				}
+				//if message.ID == "RS232.jpg" {
+				out := fmt.Sprintf("這是RS232接頭")
+				bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.ID+out+message.OriginalContentURL+message.PreviewImageURL)).Do()
+				//}
 				//out := fmt.Sprintf("這是圖片")
 				//bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(out)).Do()
 				/*if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewImageMessage(message.ID+":"+message.OriginalContentURL+message.PreviewImageURL+"ImageOK!")).Do(); err != nil {
